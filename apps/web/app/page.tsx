5a5ccff0ba1e30bddd7083c76e683ee3c6b905ee
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-04 07:36:37 +0000

    debug

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,20 +3,12 @@ import Editor from "@/ui/editor";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center sm:px-4 sm:pt-[calc(20vh)]">
+    <div className="flex flex-col items-center sm:px-4">
       <div className="flex justify-start space-x-4 m-2">
-      <button className="bg-blue-400 mt-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-  Open Docs
-  </button>
-  <button className="bg-gray-400 mt-4 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
-  Open Notion
-  </button>
-  <button className="bg-red-400 mt-4 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-  Open Coda 
-  </button>
-  <button className="bg-green-400 mt-4 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-  Open Evernote 
-  </button>
+      <button className="bg-blue-400 mt-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Open Docs</button>
+      <button className="bg-gray-400 mt-4 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Open Notion</button>
+      <button className="bg-red-400 mt-4 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">Open Coda</button>
+      <button className="bg-green-400 mt-4 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Open Evernote</button>
   </div>
       <a
         href="https://finalproject-drab.vercel.app/"
