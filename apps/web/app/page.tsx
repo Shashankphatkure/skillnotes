db4300d2d23a59bdc77d8c278bd730c81038cb52
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-04 07:17:57 +0000

    updated button

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,23 +3,17 @@ import Editor from "@/ui/editor";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
-      <div className="min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
+    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Open Google Docs
+</button>
       <a
         href="https://finalproject-drab.vercel.app/"
         target="_blank"
-        className="absolute bottom-5 left-5 z-10 rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto sm:top-5"
+        className="absolute bottom-5 left-5 z-10 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto sm:top-5"
       >
-        Open Project
+        
       </a>
-      <a
-        href="https://www.notion.so" // replace with your actual Notion link
-        target="_blank"
-        className="bg-white p-2 m-2 absolute bottom-5 left-5"
-      >
-        Open Notion
-      </a>
-    </div>
       <Menu />
       <Editor />
     </div>
