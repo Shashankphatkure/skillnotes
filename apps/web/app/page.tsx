0201e569f0846a51ef3425b4d72ef7dea59c1743
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-04 07:22:16 +0000

    updated buttons

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,9 +4,18 @@ import Editor from "@/ui/editor";
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
   Open Google Docs
-</button>
+  </button>
+  <button className="bg-blue-500 hover:bg-black-600 text-white font-bold py-2 px-4 rounded">
+  Open Notion Docs
+  </button>
+  <button className="bg-blue-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+  Open Coda 
+  </button>
+  <button className="bg-blue-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+  Open Evernote 
+  </button>
       <a
         href="https://finalproject-drab.vercel.app/"
         target="_blank"
