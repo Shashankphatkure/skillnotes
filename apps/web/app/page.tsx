6ad4821a66df4ec07cb6182a184a8931f2ea36b4
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-04 07:28:14 +0000

    updated

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,16 +5,16 @@ export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
       <div className="flex space-x-4">
-      <button className="bg-blue-200 mt-4 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
+      <button className="bg-blue-300 mt-4 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
   Open Docs
   </button>
-  <button className="bg-gray-200 mt-4 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-gray-300 mt-4 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
   Open Notion
   </button>
-  <button className="bg-red-200 mt-4 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-red-300 mt-4 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">
   Open Coda 
   </button>
-  <button className="bg-green-200 mt-4 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-green-300 mt-4 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
   Open Evernote 
   </button>
   </div>
