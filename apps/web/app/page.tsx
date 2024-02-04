122ebea561e634fa8b1aa498744378c10525e315
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-04 07:11:34 +0000

    removed flex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,8 @@ import Editor from "@/ui/editor";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
-      <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
-      <button className="bg-white p-2 m-2 absolute bottom-5 left-5">Help me</button>
+    <div className="min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
+      <div className="min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
       <a
         href="https://finalproject-drab.vercel.app/"
         target="_blank"
@@ -16,14 +15,14 @@ export default function Page() {
       <a
         href="https://docs.google.com" // replace with your actual Docs link
         target="_blank"
-        className="bg-white p-2 m-2 absolute bottom-10 left-5"
+        className="bg-white p-2 m-2 absolute bottom-5 left-5"
       >
         Open Docs
       </a>
       <a
         href="https://www.notion.so" // replace with your actual Notion link
         target="_blank"
-        className="bg-white p-2 m-2 absolute bottom-15 left-5"
+        className="bg-white p-2 m-2 absolute bottom-5 left-5"
       >
         Open Notion
       </a>
